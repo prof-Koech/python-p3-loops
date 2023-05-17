8d81ec52322b5788d186b7e2ace83a0b6cdabc86
Kennedy <kiplangatkennedy019@gmail.com>
2023-05-17 22:09:43 +0300

    second pass

--- lib/looping.py
+++ lib/looping.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+      
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_numbers=[num**2 for num in int_list]
+    return squared_numbers
 
 def fizzbuzz():
     # code goes here!
